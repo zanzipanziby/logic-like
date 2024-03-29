53312f2366692a1783d7721e2f5a762b1db075ad
ZanziPanziBY <zanzipanziby@gmail.com>
2024-03-30 01:34:16 +0300

    feat: add global styles and font

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,8 @@
 import type { Preview } from '@storybook/react'
 
+import '@fontsource-variable/nunito'
+import '../src/styles/index.scss'
+
 const preview: Preview = {
   parameters: {
     controls: {
