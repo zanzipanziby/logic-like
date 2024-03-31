5741dd393fcfe8577677428dbc69765d123324d1
ZanziPanziBY <zanzipanziby@gmail.com>
2024-04-01 02:19:02 +0300

    fix: Sidebar.stories.tsx

--- src/components/ui/Sidebar/Sidebar.stories.tsx
+++ src/components/ui/Sidebar/Sidebar.stories.tsx
@@ -17,6 +17,7 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   args: {
     activeTab: 'Все продукты',
+    setActiveTab: (_: string) => {},
     sidebarElems,
   },
   decorators: [
